f1fc0dac32e9b3aec12c4cd1fcd0857e21293106
Jose Daniel Rodriguez Morales <josedaniel@uclv.cu>
2019-01-31 16:06:00 -0500

    fix delete_unactivated command (broken from original dmoj)

--- judge/management/commands/delete_unactivated.py
+++ judge/management/commands/delete_unactivated.py
@@ -1,16 +1,16 @@
 from datetime import timedelta
 
 from django.conf import settings
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from django.utils import timezone
 from registration.models import RegistrationProfile
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = 'Delete expired user registrations from the database'
 
-    def handle_noargs(self, **options):
-        for profile in RegistrationProfile.objects.exclude(activation_key=RegistrationProfile.ACTIVATED)\
+    def handle(self,  *args,  **options):
+        for profile in RegistrationProfile.objects.exclude(activated=True)\
                       .filter(user__date_joined__lt=timezone.now() - timedelta(days=settings.ACCOUNT_ACTIVATION_DAYS))\
                       .filter(user__is_active=False):
             if not profile.user.profile.submission_set.exists():
