ff5b1f8d661bf85c22ec9717a2b11e91791a5a09
Jose Daniel Rodriguez Morales <josedaniel@uclv.cu>
2020-01-19 15:53:56 -0500

    some improvements and use big python image

--- .docker/site/boot.sh
+++ .docker/site/boot.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python3 manage.py compilemessages
-
-python3 manage.py migrate
-python3 manage.py loaddata navbar
-python3 manage.py loaddata language_small
-
+# build static files
+# ./make_style.sh
 python3 manage.py collectstatic --no-input
+python3 manage.py compilemessages
 python3 manage.py compilejsi18n
+
+# upgrade db (if any)
+python3 manage.py migrate
 
 # execute any command passed as parameter
 "$@"
