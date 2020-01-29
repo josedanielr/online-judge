f23debc770e7abdb4a63a8796bc3123e2fb3ce59
Jose Daniel Rodriguez Morales <josedaniel@uclv.cu>
2020-01-29 09:49:54 -0500

    get an initial working setup for docker stack

--- .docker/site/boot.sh
+++ .docker/site/boot.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 # build static files
-python3 manage.py collectstatic --no-input
-python3 manage.py compilemessages
-python3 manage.py compilejsi18n
+echo -e '\n\n#### Collecting static files\n'
+python manage.py collectstatic --no-input
+
+# TODO: requires gettext
+# echo -e '\n\n#### Compiling messages\n'
+# python manage.py compilemessages
+
+echo -e '\n\n#### Compiling jsi18n\n'
+python manage.py compilejsi18n
 
 # upgrade db (if any)
-python3 manage.py migrate
+echo -e '\n\n#### Running DB migrations\n'
+# python manage.py migrate
+
+# echo -e '\n\n#### Loading fixtures\n'
+# python manage.py loaddata navbar
+# python manage.py loaddata language_small
 
 # execute any command passed as parameter
 "$@"
