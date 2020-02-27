f5c806338cc84b34b0e9b19d51cae4622ada5f65
Jose D. Rodriguez <josedanielr@yandex.com>
2020-02-27 16:11:12 -0500

    aesthetic changes

--- .docker/site/boot.sh
+++ .docker/site/boot.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # build static files
-echo -e '\n\n#### Collecting static files\n'
+echo -e '\n\n### Collecting static files\n'
 python manage.py collectstatic --no-input
 
-# TODO: requires gettext
-# echo -e '\n\n#### Compiling messages\n'
+# TODO: requires gettext (this can be done on build)
+# echo -e '\n\n### Compiling messages\n'
 # python manage.py compilemessages
 
-echo -e '\n\n#### Compiling jsi18n\n'
-python manage.py compilejsi18n
+# TODO: depends on previous step
+# echo -e '\n\n### Collecting jsi18n\n'
+# python manage.py compilejsi18n
 
 # upgrade db (if any)
-echo -e '\n\n#### Running DB migrations\n'
+echo -e '\n\n### Running DB migrations\n'
 # python manage.py migrate
 
-# echo -e '\n\n#### Loading fixtures\n'
+# echo -e '\n\n### Loading fixtures\n'
 # python manage.py loaddata navbar
 # python manage.py loaddata language_small
 # python manage.py loaddata demo
+
+echo -e '\n\n### Initialization completed!\n'
 
 # execute any command passed as parameter
 "$@"
